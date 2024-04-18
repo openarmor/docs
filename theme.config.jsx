4c93e7dfc399143e9ac2a7270159ed800ff625a5
mranv <iamanubhavgain@gmail.com>
2024-04-19 01:53:45 +0530

    feat: Added division for PU & Dynamicity seo on publishing of new mdx files.

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,19 +1,93 @@
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
+
 export default {
   logo: <span>OpenArmor Documentation</span>,
   project: {
     link: "https://github.com/openarmor",
   },
   useNextSeoProps() {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://theopenarmor.org" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
     return {
-      titleTemplate: "%s – OpenArmor Documentation",
+      title: "OpenArmor and OpenArmor Dashboards",
+      description:
+        "This section contains documentation for OpenArmor and OpenArmor Dashboards.",
+      canonical: url,
+      openGraph: {
+        url: url,
+        title: "OpenArmor and OpenArmor Dashboards",
+        description:
+          "This section contains documentation for OpenArmor and OpenArmor Dashboards.",
+        images: [
+          {
+            url: "https://docs.theopenarmor.org/_next/static/media/communication.c1c738a5.png",
+            alt: "OpenArmor and OpenArmor Dashboards",
+          },
+          {
+            url: "https://docs.theopenarmor.org/_next/static/media/working-part.8b62a56d.png",
+            alt: "OpenArmor System Design",
+          },
+        ],
+        site_name: "OpenArmor Documentation",
+      },
+      twitter: {
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+      },
     };
   },
   banner: {
     key: "2.0-release",
     text: (
-      <a href="https://openarmor.com" target="_blank">
+      <a href="https://theopenarmor.org" target="_blank">
         🎉 OpenArmor 0.1 will be releasing soon. Read more →
       </a>
     ),
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://docs.theopenarmor.org" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "OpenArmor"} />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "OpenArmor & OpenArmor Dashboard Documentation"
+          }
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </>
+    );
   },
 };
